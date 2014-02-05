41bfc4d1280f6b39af4721c77853193cc47408ea
rexim <reximkut@gmail.com>
2014-02-05 09:11:40 +0700

    Show warning on bad edges.

--- main.cpp
+++ main.cpp
@@ -42,10 +42,10 @@ struct Vertex
 };
 
 using AdjacencyList =
-    std::map<int32_t, std::list<Way> >;
+	std::map<int32_t, std::list<Way> >;
 
 using VertexList =
-    std::map<int32_t, Vertex>;
+	std::map<int32_t, Vertex>;
 
 struct RoutingHeader
 {
@@ -98,28 +98,28 @@ BoundingBox get_bounding_box(const VertexList &vertex_list)
 template <typename Type>
 Type read_value(std::istream *is)
 {
-    int n = sizeof(Type);
-    char buffer[n];
+	int n = sizeof(Type);
+	char buffer[n];
 
-    for (int i = n - 1; i >= 0; --i) {
-        buffer[i] = is->get();
-    }
+	for (int i = n - 1; i >= 0; --i) {
+		buffer[i] = is->get();
+	}
 
-    return *reinterpret_cast<Type*>(buffer);
+	return *reinterpret_cast<Type*>(buffer);
 }
 
 template <typename Type>
 void skip(std::istream *is)
 {
-    read_value<Type>(is);
+	read_value<Type>(is);
 }
 
 void skip_header(std::istream *is)
 {
-    skip<int32_t>(is);
-    skip<double>(is);
-    skip<double>(is);
-    skip<int32_t>(is);
+	skip<int32_t>(is);
+	skip<double>(is);
+	skip<double>(is);
+	skip<int32_t>(is);
 }
 
 RoutingHeader read_header(std::istream *is)
@@ -132,14 +132,22 @@ RoutingHeader read_header(std::istream *is)
 	return result;
 }
 
+void print_header(std::istream *is, const RoutingHeader &header)
+{
+	std::cout << header.transfer_time << std::endl;
+	std::cout << header.pedestrian_speed << std::endl;
+	std::cout << header.convertion_ratio << std::endl;
+	std::cout << header.max_distance << std::endl;
+}
+
 void read_adjacency_list(std::istream *is, AdjacencyList *adjacency_list)
 {
-    int32_t n = read_value<int32_t>(is);
-    for (int32_t i = 0; i < n; ++i) {
-        int32_t u = read_value<int32_t>(is);
-        int32_t m = read_value<int32_t>(is);
+	int32_t n = read_value<int32_t>(is);
+	for (int32_t i = 0; i < n; ++i) {
+		int32_t u = read_value<int32_t>(is);
+		int32_t m = read_value<int32_t>(is);
 
-        for (int32_t j = 0; j < m; ++j) {
+		for (int32_t j = 0; j < m; ++j) {
 			Way way;
 
 			way.destination = read_value<int32_t>(is);
@@ -147,25 +155,25 @@ void read_adjacency_list(std::istream *is, AdjacencyList *adjacency_list)
 			way.type = read_value<int32_t>(is);
 			way.weight = read_value<int32_t>(is);
 
-            (*adjacency_list)[u].push_back(way);
-        }
-    }
+			(*adjacency_list)[u].push_back(way);
+		}
+	}
 }
 
 void read_vertex_list(std::istream *is, VertexList *vertex_list)
 {
-    int32_t n = read_value<int32_t>(is);
+	int32_t n = read_value<int32_t>(is);
 
-    for (int32_t i = 0; i < n; ++i) {
-        int32_t u = read_value<int32_t>(is);
+	for (int32_t i = 0; i < n; ++i) {
+		int32_t u = read_value<int32_t>(is);
 
 		Vertex vertex;
 		vertex.station_id = read_value<int64_t>(is);
-        vertex.x = read_value<double>(is) * 0.01;
-        vertex.y = read_value<double>(is) * 0.01;
+		vertex.x = read_value<double>(is) * 0.01;
+		vertex.y = read_value<double>(is) * 0.01;
 
-        (*vertex_list)[u] = vertex;
-    }
+		(*vertex_list)[u] = vertex;
+	}
 }
 
 void dump_graph_to_png_file(const AdjacencyList &adjacency_list,
@@ -188,9 +196,21 @@ void dump_graph_to_png_file(const AdjacencyList &adjacency_list,
 	cairo_set_line_width(cr.get(), 1.0);
 
 	for (const auto &edge: adjacency_list) {
-		auto u = vertex_list.at(std::get<0>(edge));
+		auto u = vertex_list.find(std::get<0>(edge));
+
+		if (u == vertex_list.end()) {
+			std::cout << "[WARNING] " << std::endl;
+			continue;
+		}
+
 		for (const auto &way: std::get<1>(edge)) {
-			auto v = vertex_list.at(way.destination);
+			auto v = vertex_list.find(way.destination);
+
+			if (v == vertex_list.end()) {
+				std::cout << "[WARNING] " << std::endl;
+				continue;
+			}
+
 			switch (way.type) {
 			case 0:
 				cairo_set_source_rgb(cr.get(), 1.0, 0.0, 0.0);
@@ -208,8 +228,8 @@ void dump_graph_to_png_file(const AdjacencyList &adjacency_list,
 				std::cout << "Wrong type" << std::endl;
 			}
 
-			cairo_move_to(cr.get(), u.x - bbox.minx, u.y - bbox.miny);
-			cairo_line_to(cr.get(), v.x - bbox.minx, v.y - bbox.miny);
+			cairo_move_to(cr.get(), u->second.x - bbox.minx, u->second.y - bbox.miny);
+			cairo_line_to(cr.get(), v->second.x - bbox.minx, v->second.y - bbox.miny);
 			cairo_stroke(cr.get());
 		}
 	}
@@ -226,33 +246,24 @@ double distance(const Vertex &p1, const Vertex &p2)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        std::cerr << "Usage: ./routing-drawer routing.bin map.png" << std::endl;
-        return EXIT_FAILURE;
-    }
+	if (argc < 3) {
+		std::cerr << "Usage: ./routing-drawer routing.bin map.png" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    try {
-        std::ifstream fin(argv[1], std::ifstream::in | std::ifstream::binary);
-		auto header = read_header(&fin);
+	std::ifstream fin(argv[1], std::ifstream::in | std::ifstream::binary);
+	auto header = read_header(&fin);
+	std::cout << "----------------------------------------" << std::endl;
+	print_header(&fin, header);
+	std::cout << "----------------------------------------" << std::endl;
 
-        std::cout << "----------------------------------------" << std::endl;
-        std::cout << header.transfer_time << std::endl;
-        std::cout << header.pedestrian_speed << std::endl;
-        std::cout << header.convertion_ratio << std::endl;
-        std::cout << header.max_distance << std::endl;
-        std::cout << "----------------------------------------" << std::endl;
+	AdjacencyList adjacency_list;
+	read_adjacency_list(&fin, &adjacency_list);
 
-        AdjacencyList adjacency_list;
-        read_adjacency_list(&fin, &adjacency_list);
+	VertexList vertex_list;
+	read_vertex_list(&fin, &vertex_list);
 
-        VertexList vertex_list;
-        read_vertex_list(&fin, &vertex_list);
+	dump_graph_to_png_file(adjacency_list, vertex_list, argv[2]);
 
-        dump_graph_to_png_file(adjacency_list, vertex_list, argv[2]);
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
