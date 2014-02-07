76ea2c025ae4108f11f3b1109254804f0f3af7a4
rexim <reximkut@gmail.com>
2014-02-07 11:15:42 +0700

    Output metainfo.

--- main.cpp
+++ main.cpp
@@ -134,10 +134,10 @@ RoutingHeader read_header(std::istream *is)
 
 void print_header(std::istream *is, const RoutingHeader &header)
 {
-	std::cout << header.transfer_time << std::endl;
-	std::cout << header.pedestrian_speed << std::endl;
-	std::cout << header.convertion_ratio << std::endl;
-	std::cout << header.max_distance << std::endl;
+	std::cout << "Transfer Time: " << header.transfer_time << std::endl;
+	std::cout << "Pedestrian Speed: " << header.pedestrian_speed << std::endl;
+	std::cout << "Convertion Ratio: " << header.convertion_ratio << std::endl;
+	std::cout << "Max Distance: " << header.max_distance << std::endl;
 }
 
 void read_adjacency_list(std::istream *is, AdjacencyList *adjacency_list)
@@ -146,6 +146,13 @@ void read_adjacency_list(std::istream *is, AdjacencyList *adjacency_list)
 	for (int32_t i = 0; i < n; ++i) {
 		int32_t u = read_value<int32_t>(is);
 		int32_t m = read_value<int32_t>(is);
+
+		// Ensuring that even empty rows of adjacency list will be
+		// created, so adjacency_list->size() is actual amount of
+		// rows.
+		if (adjacency_list->find(u) == adjacency_list->end()) {
+			adjacency_list->insert(std::make_pair(u, std::list<Way>()));
+		}
 
 		for (int32_t j = 0; j < m; ++j) {
 			Way way;
@@ -253,15 +260,17 @@ int main(int argc, char *argv[])
 
 	std::ifstream fin(argv[1], std::ifstream::in | std::ifstream::binary);
 	auto header = read_header(&fin);
-	std::cout << "----------------------------------------" << std::endl;
 	print_header(&fin, header);
-	std::cout << "----------------------------------------" << std::endl;
 
 	AdjacencyList adjacency_list;
 	read_adjacency_list(&fin, &adjacency_list);
 
+	std::cout << "Size of Adjacency List: " << adjacency_list.size() << std::endl;
+
 	VertexList vertex_list;
 	read_vertex_list(&fin, &vertex_list);
+
+	std::cout << "Size of Vertex List: " << vertex_list.size() << std::endl;
 
 	dump_graph_to_png_file(adjacency_list, vertex_list, argv[2]);
 
